78c7422ef791be39d65505e10c1e5f89af625fd1
summerfaliero <summerfaliero@gmail.com>
2024-03-25 17:05:48 -0400

    resolving merge conflicts and adding sqlite target to cmakelists.txt

--- PurrrfectSpots/cmake-build-debug/CMakeFiles/PurrrfectSpots.dir/DependInfo.cmake
+++ PurrrfectSpots/cmake-build-debug/CMakeFiles/PurrrfectSpots.dir/DependInfo.cmake
@@ -8,13 +8,9 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-<<<<<<< HEAD
   "/Users/summerfaliero/coral/PurrrfectSpots/Demo.cpp" "CMakeFiles/PurrrfectSpots.dir/Demo.cpp.o" "gcc" "CMakeFiles/PurrrfectSpots.dir/Demo.cpp.o.d"
+  "/Users/summerfaliero/coral/PurrrfectSpots/Reservations.cpp" "CMakeFiles/PurrrfectSpots.dir/Reservations.cpp.o" "gcc" "CMakeFiles/PurrrfectSpots.dir/Reservations.cpp.o.d"
   "/Users/summerfaliero/coral/PurrrfectSpots/main.cpp" "CMakeFiles/PurrrfectSpots.dir/main.cpp.o" "gcc" "CMakeFiles/PurrrfectSpots.dir/main.cpp.o.d"
-=======
-  "/home/hallake/cs205/coral/PurrrfectSpots/Reservations.cpp" "CMakeFiles/PurrrfectSpots.dir/Reservations.cpp.o" "gcc" "CMakeFiles/PurrrfectSpots.dir/Reservations.cpp.o.d"
-  "/home/hallake/cs205/coral/PurrrfectSpots/main.cpp" "CMakeFiles/PurrrfectSpots.dir/main.cpp.o" "gcc" "CMakeFiles/PurrrfectSpots.dir/main.cpp.o.d"
->>>>>>> 57de89f70b4ef485d45288c437f8eda5fa19f961
   )
 
 # Targets to which this target links which contain Fortran sources.
