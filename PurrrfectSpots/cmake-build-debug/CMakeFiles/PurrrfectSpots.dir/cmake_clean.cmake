2c58f77ef984a99849e46cd608a88b907724f6da
hallake <hallake@lafayette.edu>
2024-03-25 15:05:40 -0400

    trying to push main.cpp

---
+++ PurrrfectSpots/cmake-build-debug/CMakeFiles/PurrrfectSpots.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PurrrfectSpots.dir/main.cpp.o"
+  "CMakeFiles/PurrrfectSpots.dir/main.cpp.o.d"
+  "PurrrfectSpots"
+  "PurrrfectSpots.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PurrrfectSpots.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
