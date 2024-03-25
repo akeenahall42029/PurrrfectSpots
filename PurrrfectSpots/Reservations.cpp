4a2a989a83abde596b1607ea0431520f451b8624
hallake <hallake@lafayette.edu>
2024-03-25 16:28:09 -0400

    Created skeleton of setters and getters for Reservations

--- PurrrfectSpots/Reservations.cpp
+++ PurrrfectSpots/Reservations.cpp
@@ -4,14 +4,9 @@
 
 #include "Reservations.h"
 #include <random>
-int Reservations::generateID() {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> distrib(100000000, 999999999);
-
-    return distrib(gen);
-}
-
+#include <sqlite3.h>
+#include "iostream"
+// constructor
 Reservations::Reservations(const std::string &napSpotID, const std::string &userName) {
     id = generateID();
 
@@ -21,3 +16,76 @@ Reservations::Reservations(const std::string &napSpotID, const std::string &user
  * a new reservation
  * @return randomly generated id number */
 
+int Reservations::generateID() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(100000000, 999999999);
+
+    return distrib(gen);
+}
+
+std::string Reservations::getNapSpotId() const {
+    return std::string();
+}
+
+std::string Reservations::getUserName() const {
+    return std::string();
+}
+
+int Reservations::getTime() {
+    return 0;
+}
+
+int Reservations::getID() const {
+    return id; // id is defined as a private variable in the header, return to this if this causes problems
+
+}
+std::string Reservations::getUserId() const {
+    return std::string();
+}
+
+std::string Reservations::getStatus() {
+    return std::string();
+}
+
+void Reservations::setNapSpotId(int napSpotId) {
+
+}
+
+void Reservations::setUserName(std::string userName) {
+
+}
+
+void Reservations::setUserId(std::string userId) {
+
+}
+
+/* Bonds connection between userInput when a user creates a new reservation and adds
+ * an entry to the reservations table.
+ * @param reservation
+ * */
+
+
+void storeData(const Reservations& reservation){
+    sqlite3* db; // access sqlite database using a pointer
+    int rc = sqlite3_open("build_tables.sql", &db); // rc--> return code
+    if(rc) {
+        std::cerr << "Error opening SQlite database" << sqlite3_errmsg(db) << std::endl;
+        return;
+
+    }
+
+    //Prepare the sqlite statement to add info to database
+    sqlite3_stmt* sql_statement;
+    const char* sql = "INSERT INTO reservations (id, napSpotId, userId, time, status) VALUES (?, ?, ?, ?, ?)";
+    rc = sqlite3_prepare_v2(db,sql,-1,&sql_statement,NULL); // prepare parameters: database handle, sql statement, max length of bytes, pointer to statement
+    if(rc !=SQLITE_OK){
+        std::cerr << "Error preparing SQL statement" << sqlite3_errmsg(db) << std::endl;
+        sqlite3_close(db);
+        return;
+    }
+
+    // Bind paremeters to SQL statement NOTE: this is more secure than Sql insections
+   //  sqlite3_bind_int(sql_statement,1,reservation.getID())
+
+}
--- PurrrfectSpots/Reservations.h
+++ PurrrfectSpots/Reservations.h
@@ -12,13 +12,26 @@ private:
     std::string napSpotId; // matches name in reservations database
     std::string userId;
     std::string userName;
+    int time ;
+    std::string status;
 
     // Function to generate random id number for the reservation
     int generateID();
+    void setNapSpotId(int napSpotId);
+    void setUserName(std::string userName);
+    void setUserId(std::string userId);
 
 public:
     //Constructor
     Reservations(const std:: string& napSpotID, const std::string& userName);
+    // Getter functions, will be useful for SQL binding
+    int getID() const; // Change the return type to int
+    std::string getNapSpotId() const;
+    std::string getUserName() const;
+    std::string getUserId() const;
+    int getTime();
+    std::string getStatus();
+
 
 
 
