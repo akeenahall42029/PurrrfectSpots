ba3881f8d02975ef0d601bc3d730829f41710a77
hiness_mac <hiness@lafayette.edu>
2024-04-29 16:18:37 -0400

    Database unit tests work and then i added the picture and changed the color

---
+++ PurrrfectSpots/unit_tests/UserDBTests.cpp
@@ -0,0 +1,39 @@
+////
+//// Created by Sanaa Hines on 4/29/24.
+////
+//#include <gtest/gtest.h>
+//#include "../database/Database.h"
+//
+//// Test fixture for Database class
+//class DatabaseTest : public ::testing::Test {
+//protected:
+//    // Set up the test case
+//    void SetUp() override {
+//        // Create an instance of the Database class
+//        db = new Database();
+//    }
+//
+//    // Clean up after the test case
+//    void TearDown() override {
+//        // Delete the Database instance
+//        delete db;
+//    }
+//
+//    // Pointer to the Database instance
+//    Database* db;
+//};
+//
+//
+//// Test the results method of the Database class
+//TEST_F(DatabaseTest, ResultsTest) {
+//    // Query the database for some information
+//    std::string query = "SELECT * FROM some_table;";
+//    std::vector<std::string> results = db->results(query);
+//
+//    // Check if the results vector is not empty
+//    EXPECT_FALSE(results.empty());
+//
+//    // You can add more specific assertions based on the expected results
+//    // For example, check the size of the results vector or the content of specific rows
+//}
+
