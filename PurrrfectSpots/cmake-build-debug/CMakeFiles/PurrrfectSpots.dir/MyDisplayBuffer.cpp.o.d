f4ae5d9c70e848daffb86806d53ea2f562584c22
hiness_mac <hiness@lafayette.edu>
2024-04-29 10:16:24 -0400

    cleaning up .idea and cmake_build...

--- PurrrfectSpots/cmake-build-debug/CMakeFiles/PurrrfectSpots.dir/MyDisplayBuffer.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/PurrrfectSpots.dir/MyDisplayBuffer.cpp.o: \
-  /Users/summerfaliero/coral/PurrrfectSpots/MyDisplayBuffer.cpp \
-  /Users/summerfaliero/coral/PurrrfectSpots/MyDisplayBuffer.h
